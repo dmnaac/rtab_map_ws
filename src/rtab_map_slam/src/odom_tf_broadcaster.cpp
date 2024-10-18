d283c5ee21746bda5ac18997fb041aca4cf8f4cf
dmnaac <dmnaac@hotmail.com>
2024-10-18 18:19:58 +0800

    Modified.

--- src/rtab_map_slam/src/odom_tf_broadcaster.cpp
+++ src/rtab_map_slam/src/odom_tf_broadcaster.cpp
@@ -3,12 +3,16 @@
 #include <geometry_msgs/TransformStamped.h>
 #include <nav_msgs/Odometry.h>
 
-tf2_ros::TransformBroadcaster broadcaster;
+double translation_x;
+double translation_y;
+double translation_z;
+double rotation_x;
+double rotation_y;
+double rotation_z;
+double rotation_w;
 
 void CallBack(const nav_msgs::Odometry::ConstPtr& msg)
 {
-    geometry_msgs::TransformStamped odom_trans;
-
     double translation_x = msg->pose.pose.position.x;
     double translation_y = msg->pose.pose.position.y;
     double translation_z = msg->pose.pose.position.z;
@@ -16,10 +20,19 @@ void CallBack(const nav_msgs::Odometry::ConstPtr& msg)
     double rotation_y = msg->pose.pose.orientation.y;
     double rotation_z = msg->pose.pose.orientation.z;
     double rotation_w = msg->pose.pose.orientation.w;
+}
 
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "odom_tf_broadcaster");
+    ros::NodeHandle nodehandle;
+    tf2_ros::TransformBroadcaster broadcaster;
+    ros::Subscriber sub = nodehandle.subscribe("/odom", 1, &CallBack);
+    
+    geometry_msgs::TransformStamped odom_trans;
     odom_trans.header.stamp = ros::Time::now();
-    odom_trans.header.frame_id = "odom_combined";
-    odom_trans.child_frame_id = "base_footprint";
+    odom_trans.header.frame_id = "laser";
+    odom_trans.child_frame_id = "odom_new";
 
     odom_trans.transform.translation.x = translation_x;
     odom_trans.transform.translation.y = translation_y;
@@ -30,13 +43,7 @@ void CallBack(const nav_msgs::Odometry::ConstPtr& msg)
     odom_trans.transform.rotation.w = rotation_w;
 
     broadcaster.sendTransform(odom_trans);
-}
 
-int main(int argc, char** argv)
-{
-    ros::init(argc, argv, "odom_tf_broadcaster");
-    ros::NodeHandle nodehandle;
-    ros::Subscriber sub = nodehandle.subscribe("/odom", 10, &CallBack);
     ros::spin();
     return 0;
 }
